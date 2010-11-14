a7dc1c208abfacbf92617746a577fc3be432be2b
Denise H. G <darcsis@gmail.com>
2010-11-15 03:56:39 +0800

    code cleanup.

--- btree/bstree.c
+++ btree/bstree.c
@@ -10,17 +10,17 @@ bst_mknode(const char *str)
 {
   bst_node *np;
   
-  if (str && (np = malloc(sizeof(bst_node)))) {
-
-	bzero(np->node, BST_ENT_SIZE);
-	strlcpy(np->node, str, strlen(str) + 1);
-	np->left = NULL;
-	np->right = NULL;
-	np->deleted = 0;
-	return (np);
-  } else {
+  if (str == NULL)
 	return (NULL);
-  }
+  if ((np = malloc(sizeof(bst_node))) == NULL)
+	return (NULL);
+
+  bzero(np->node, BST_ENT_SIZE);
+  strlcpy(np->node, str, strlen(str) + 1);
+  np->left = NULL;
+  np->right = NULL;
+  np->deleted = 0;
+  return (np);
 }
 
 BSTREE *
@@ -28,22 +28,23 @@ bst_init(void)
 {
   BSTREE *tp;
 			  
-  if ((tp = malloc(sizeof(BSTREE)))) {
-	tp->root = NULL;
-	tp->size = 0;
-  }
-
+  if ((tp = malloc(sizeof(BSTREE))) == NULL)
+	return (NULL);
+  
+  tp->root = NULL;
+  tp->size = 0;
   return (tp);
 }
   
 int
 bst_empty(BSTREE *tp)
 {
-  if (tp) {
-	if (tp->root == NULL &&
-		tp->size == 0)
-	  return (1);
-  }
+  if (tp == NULL)
+	return (1);
+  
+  if (tp->root == NULL &&
+	  tp->size == 0)
+	return (1);
 
   return (0);
 }
@@ -51,6 +52,13 @@ bst_empty(BSTREE *tp)
 bst_node *
 bst_find(const char *str, BSTREE *tp)
 {
+  if (str == NULL)
+	return (NULL);
+  if (tp == NULL)
+	return (NULL);
+  if (bst_empty(tp))
+	return (NULL);
+  
   return (_bst_loc(str, tp->root));
 }
 
@@ -60,90 +68,94 @@ _bst_loc(const char *str, bst_node *n)
   int m;
   bst_node *cur, *np;
   
+  if (str == NULL)
+	return (NULL);
+  if (n == NULL)
+	return (NULL);
+  
   cur = np = n;
   
-  if (np) {
-
-	if (!np->deleted) {
-	  
-	  m = strncmp(str, np->node, strlen(str) + 1);
+  if (np->deleted != 1) {
+	m = strncmp(str, np->node, strlen(str) + 1);
 	
-	  if (0 == m)
-		return (np);
-	
-	  if (0 > m) {
-		if (np->left)
-		  cur = np = _bst_loc(str, np->left);
-		else
-		  return (np);
-	  }
-	  
-	  if (0 < m) {
-		if (np->right)
-		  cur = np = _bst_loc(str, np->right);
-		else
-		  return (np);
-	  }
-
+	if (0 == m)
 	  return (np);
+	
+	if (0 > m) {
+	  if (np->left != NULL)
+		cur = np = _bst_loc(str, np->left);
+	  else
+		return (np);
 	}
+	  
+	if (0 < m) {
+	  if (np->right != NULL)
+		cur = np = _bst_loc(str, np->right);
+	  else
+		return (np);
+	}
+	
+	return (np);
   }
   
   return (cur);
 }
 
-void
-bst_ins(const char *str, BSTREE *tp, int ic)
+int
+bst_ins(const char *str, BSTREE *tp, const int ic)
 {
-  if (!bst_empty(tp)) {
-	_bst_ins(str, tp->root, ic);
-	tp->size++;
-	return;
-  }
+  if (str == NULL)
+	return (0);
+  if (tp == NULL)
+	return (0);
 
-  if (tp) {
-	if ((tp->root = bst_mknode(str))) {
-	  tp->size++;
-	  return;
-	}
+  if (bst_empty(tp)) {
+
+	if ((tp->root = bst_mknode(str)) == NULL)
+	  return (0);
+	
+	tp->size++;
+	return (1);
   }
+  
+  _bst_ins(str, tp->root, ic);
+  tp->size++;
+  return (1);
 }
 
 static void
-_bst_ins(const char *str, bst_node *pos, int ic)
+_bst_ins(const char *str, bst_node *pos, const int ic)
 {
   int m;
   bst_node *np;
 
-  np = _bst_loc(str, pos);
+  if (str == NULL)
+	return;
+  if (pos == NULL)
+	return;
   
-  if (np) {
+  np = _bst_loc(str, pos);
+  m = strncmp(str, np->node, strlen(str) + 1);
 
-	m = strncmp(str, np->node, strlen(str) + 1);
+  if (ic == 1 && m == 0) {	  
+	if (np->left != NULL)		
+	  np->left = bst_mknode(str);
+	else
+	  _bst_ins(str, np->left, ic);
+  }
 
-	if (ic == 1 && m == 0) {
-	  
-	  if (!np->left)		
-		np->left = bst_mknode(str);
-	  else
-		_bst_ins(str, np->left, ic);
-	}
-
-	if (0 > m) {
-	  
-	  if (!np->left)
-		np->left = bst_mknode(str);
-	  else
-		_bst_ins(str, np->left, ic);
-	}
+  if (0 > m) {	  
+	if (np->left != NULL)
+	  np->left = bst_mknode(str);
+	else
+	  _bst_ins(str, np->left, ic);
+  }
 	
-	if (0 < m) {
-	  
-	  if (!np->right)
-		np->right = bst_mknode(str);
-	  else
-		_bst_ins(str, np->right, ic);
-	}
+  if (0 < m) {	  
+	if (np->right != NULL)
+	  np->right = bst_mknode(str);
+	else
+	  _bst_ins(str, np->right, ic);
   }
 }
 
@@ -152,9 +164,14 @@ bst_del(const char *str, BSTREE *tp)
 {
   bst_node *np;
 
+  if (str == NULL)
+	return (0);
+  if (tp == NULL)
+	return (0);
+  
   np = _bst_loc(str, tp->root);
 
-  if (np) {
+  if (np != NULL) {
 	np->deleted = 1;
 	tp->size--;
 	return (1);
@@ -166,32 +183,37 @@ bst_del(const char *str, BSTREE *tp)
 void
 bst_free(BSTREE *tp)
 {
-  if (tp) {
-	_bst_free(tp->root);
-	free(tp);
-	tp = NULL;
-  }
+  if (tp == NULL)
+	return;
+  
+  _bst_free(tp->root);
+  free(tp);
+  tp = NULL;
 }
 
 static void
 _bst_free(bst_node *np)
 {
-  if (np) {
-
-	if (np->left)
-	  _bst_free(np->left);
-	if (np->right)
-	  _bst_free(np->right);
-
-	free(np);
-	np = NULL;
+  if (np == NULL)
 	return;
-  }
+  
+  if (np->left != NULL)
+	_bst_free(np->left);
+  if (np->right != NULL)
+	_bst_free(np->right);
+
+  free(np);
+  np = NULL;
 }
 
 void
 bst_proc(BSTREE *tp, BST_TRV_ORDER odr, void (*func_p) (const bst_node *np))
 {
+  if (tp == NULL)
+	return;
+  if (func_p == NULL)
+	return;
+  
   _bst_proc(tp->root, odr, func_p);
 }
 
@@ -200,38 +222,38 @@ _bst_proc(bst_node *beg, BST_TRV_ORDER odr, void (*func_p) (const bst_node *np))
 {
     bst_node *np;
 
-	if (beg && func_p) {
-	  
-	  np = beg;
-	  
-	  if (np) {
-		switch (odr) {
-		case BST_PREORDER:
-		  if (!np->deleted)
-			func_p(np);
-		  if (np->left)
-			_bst_proc(np->left, BST_PREORDER, func_p);
-		  if (np->right)
-			_bst_proc(np->right, BST_PREORDER, func_p);
-		  break;
-		case BST_INORDER:
-		  if (np->left)
-			_bst_proc(np->left, BST_INORDER, func_p);
-		  if (!np->deleted)
-			func_p(np);
-		  if (np->right)
-			_bst_proc(np->right, BST_INORDER, func_p);
-		  break;
-		default:
-		case BST_POSTORDER:
-		  if (np->left)
-			_bst_proc(np->left, BST_POSTORDER, func_p);
-		  if (np->right)
-			_bst_proc(np->right, BST_POSTORDER, func_p);
-		  if (!np->deleted)
-			func_p(np);
-		  break;
-		}
-	  }
+	if (beg == NULL)
+	  return;
+	if (func_p == NULL)
+	  return;
+		  
+	np = beg;
+	
+	switch (odr) {
+	case BST_PREORDER:
+	  if (np->deleted != 1)
+		func_p(np);
+	  if (np->left != NULL)
+		_bst_proc(np->left, BST_PREORDER, func_p);
+	  if (np->right != NULL)
+		_bst_proc(np->right, BST_PREORDER, func_p);
+	  break;
+	case BST_INORDER:
+	  if (np->left != NULL)
+		_bst_proc(np->left, BST_INORDER, func_p);
+	  if (np->deleted != 1)
+		func_p(np);
+	  if (np->right != NULL)
+		_bst_proc(np->right, BST_INORDER, func_p);
+	  break;
+	default:
+	case BST_POSTORDER:
+	  if (np->left != NULL)
+		_bst_proc(np->left, BST_POSTORDER, func_p);
+	  if (np->right != NULL)
+		_bst_proc(np->right, BST_POSTORDER, func_p);
+	  if (np->deleted != 1)
+		func_p(np);
+	  break;
 	}
 }
--- list/dlist.c
+++ list/dlist.c
@@ -1,6 +1,6 @@
 #include "dlist.h"
 
-/* static void _dl_ins_after(dl_node *, dl_node *); */
+static void _dl_ins_after(dl_node *, dl_node *);
 static void _dl_ins_before(dl_node *, dl_node *);
 
 dl_node *
@@ -8,16 +8,18 @@ dl_mknode(const char *str)
 {
   dl_node *np;
   
-  if (str && (np = malloc(sizeof(dl_node)))) {
-	bzero(np->node, DL_ENT_SIZE);
-	strncpy(np->node, str, strlen(str) + 1);
-	np->deleted = 0;
-	np->pre = NULL;
-	np->next = NULL;
-	return (np);
-  }
-
-  return (NULL);
+  if (str == NULL)
+	return (NULL);
+  
+  if ((np = malloc(sizeof(dl_node))) == NULL)
+	return (NULL);
+  
+  bzero(np->node, DL_ENT_SIZE);
+  strncpy(np->node, str, strlen(str) + 1);
+  np->deleted = 0;
+  np->pre = NULL;
+  np->next = NULL;
+  return (np);
 }
 
 DLIST *
@@ -25,29 +27,29 @@ dl_init(void)
 {
   DLIST *dl;
   
-  if ((dl = malloc(sizeof(DLIST)))) {
-	dl->head = NULL;
-	dl->tail = NULL;
-	dl->cur = NULL;
-	dl->len = 0;
-	return (dl);
-  }
-
-  return (NULL);
+  if ((dl = malloc(sizeof(DLIST))) == NULL)
+	return (NULL);
+  
+  dl->head = NULL;
+  dl->tail = NULL;
+  dl->cur = NULL;
+  dl->len = 0;
+  return (dl);
 }
 
 int
 dl_empty(const DLIST *dl)
 {
-  if (dl) {	
-	if (dl->head == NULL
-		&& dl->tail == NULL
-		&& dl->cur == NULL
-		&& dl->len == 0) {
-	  return (1);
-	}
+  if (dl == NULL)
+	return (1);
+
+  if ((dl->head == NULL) &&
+	  (dl->tail == NULL) &&
+	  (dl->cur == NULL) &&
+	  (dl->len == 0)) {
+	return (1);
   }
-  
+
   return (0);
 }
 
@@ -56,42 +58,40 @@ dl_append(const char *str, DLIST *dl)
 {
   dl_node *new;
   
-  if (dl) {
-	
-	new = dl_mknode(str);
+  if (dl == NULL)
+	return (0);
 
-	if (dl_empty(dl)) {
-	  dl->head = new;
-	} else {
-	  dl->tail->next = new;
-	  new->pre = dl->tail;
-	}
-	
-	dl->cur = new;
-	dl->tail = new;
-	dl->len++;
-	return (1);
+  new = dl_mknode(str);
+
+  if (dl_empty(dl)) {
+	dl->head = new;
+  } else {
+	dl->tail->next = new;
+	new->pre = dl->tail;
   }
-
-  return (0);
+	
+  dl->cur = new;
+  dl->tail = new;
+  dl->len++;
+  return (1);
 }
 
-/*
 static void
 _dl_ins_after(dl_node *np, dl_node *new)
 {
   new->pre = np;
-  new->next = np->next;
-  np->next->pre = new;
+  if (np->next != NULL) {
+	new->next = np->next;
+	np->next->pre = new;
+  }
   np->next = new;
 }
-*/
 
 static void
 _dl_ins_before(dl_node *np, dl_node *new)
 {
   new->next = np;
-  if (np->pre) {	
+  if (np->pre != NULL) {	
 	new->pre = np->pre;
 	np->pre->next = new;
   }
@@ -99,80 +99,81 @@ _dl_ins_before(dl_node *np, dl_node *new)
 }
 
 int
-dl_ins_at_pos(const char *str, int pos, DLIST *dl)
+dl_ins_at_pos(const char *str, int pos, DLIST *dl, const int before)
 {
   int i;
   dl_node *new, *np;
   
-  if (str && dl) {
-	
-	new = dl_mknode(str);
-	
-	if (dl_empty(dl)) {
+  if (str == NULL)
+	return (0);
 
-	  dl->cur = dl->head = dl->tail = new;
+  if (dl == NULL)
+	return (0);
+	
+  new = dl_mknode(str);
+	
+  if (dl_empty(dl)) {
+	dl->cur = dl->head = dl->tail = new;
+	dl->len++;
+  } else {
+	dl->cur = new;
+	if (pos <= 0) {
+	  new->next = dl->head;
+	  dl->head->pre = new;
+	  dl->head = new;
 	  dl->len++;
-	} else {
-
-	  dl->cur = new;
-	  
-	  if (pos <= 0) {
-
-		new->next = dl->head;
-		dl->head->pre = new;
-		dl->head = new;
-		dl->len++;
-	  }
-
-	  if (pos > 0 && pos < dl->len) {
-
-		np = dl->head;
-
-		for (i = 0; i < pos && np; i++)
-		  np = np->next;
-
-		_dl_ins_before(np, new);
-		dl->len++;
-	  }
-
-	  if (pos >= dl->len) {
-	  
-		new->pre = dl->tail->pre;
-		dl->tail->next = new;
-		dl->tail = new;
-		dl->len++;
-	  }
 	}
 
-	return (1);
+	if (pos > 0 && pos < dl->len) {
+	  np = dl->head;
+	  for (i = 0; i < pos && np; i++)
+		np = np->next;
+
+	  if (before == 1)
+		_dl_ins_before(np, new);
+	  else
+		_dl_ins_after(np, new);
+	  
+	  dl->len++;
+	}
+
+	if (pos >= dl->len) {
+	  new->pre = dl->tail->pre;
+	  dl->tail->next = new;
+	  dl->tail = new;
+	  dl->len++;
+	}
   }
 
-  return (0);
+  return (1);
 }
 
 int
-dl_ins_at_val(const char *str, const char *pos, DLIST *dl)
+dl_ins_at_val(const char *str, const char *pos, DLIST *dl, const int before)
 {
   dl_node *new, *np;
   
-  if (str && pos && dl) {
+  if ((str == NULL) ||
+	  (pos == NULL))
+	return (0);
 
-	dl->cur = new = dl_mknode(str);
+  if (dl == NULL)
+	return (0);
+
+  dl->cur = new = dl_mknode(str);
 	
-	if (dl_empty(dl)) {
-	  
-	  dl->head = dl->tail = new;
-	  dl->len++;
-	  return (1);
-	}
-
+  if (dl_empty(dl)) {
+	dl->head = dl->tail = new;
+	dl->len++;
+  } else {
 	np = dl->head;
-	
 	while (np) {
-	  
 	  if (0 == strncmp(pos, np->node, strlen(pos) + 1)) {
-		_dl_ins_before(np, new);
-
+		if (before == 1)
+		  _dl_ins_before(np, new);
+		else
+		  _dl_ins_after(np, new);
+		
 		if (np == dl->head)
 		  dl->head = new;
 		if (np == dl->tail)
@@ -181,21 +182,22 @@ dl_ins_at_val(const char *str, const char *pos, DLIST *dl)
 		dl->len++;
 		break;
 	  }
-	  
 	  np = np->next;
 	}
-
-	return (1);
   }
-
-  return (0);
+  return (1);
 }
 
 int
 dl_delete(const char *str, DLIST *dl)
 {  
-  if (str && !dl_empty(dl)) {
-	
+  if (str == NULL)
+	return (0);
+
+  if (dl == NULL)
+	return (0);
+  
+  if (!dl_empty(dl)) {
 	dl->cur = dl->head;
 	while (dl->cur) {
 	  if (0 == strncmp(str, dl->cur->node, strlen(str) + 1)) {
@@ -205,22 +207,26 @@ dl_delete(const char *str, DLIST *dl)
 	  }
 	  dl->cur = dl->cur->next;
 	}
+	return (1);
   }
-
   return (0);
 }
 
 void
 dl_proc(DLIST *dl, void (*func_p) (const dl_node *np))
 {
-  if (dl && func_p) {
-	if (!dl_empty(dl)) {
-	  dl->cur = dl->head;
-	  while (dl->cur) {
-		if (!dl->cur->deleted)
-		  func_p(dl->cur);
-		dl->cur = dl->cur->next;
-	  }
+  if (dl == NULL)
+	return;
+
+  if (func_p == NULL)
+	return;
+
+  if (!dl_empty(dl)) {
+	dl->cur = dl->head;
+	while (dl->cur) {
+	  if (dl->cur->deleted != 1)
+		func_p(dl->cur);
+	  dl->cur = dl->cur->next;
 	}
   }
 }
@@ -230,15 +236,16 @@ dl_free(DLIST *dl)
 {
   dl_node *np;
   
-  if (!dl) {
-	
-	np = dl->tail;
-	dl->cur = dl->tail->pre;
-	while (np) {
-	  free(np);
-	  np = NULL;
-	  np = dl->cur;
-	  dl->cur = dl->cur->pre;
-	}
+  if (dl == NULL)
+	return;
+
+  np = dl->tail;
+  dl->cur = dl->tail->pre;
+  
+  while (np) {
+	free(np);
+	np = NULL;
+	np = dl->cur;
+	dl->cur = dl->cur->pre;
   }
 }
